5b09708e4f8ad48507b9b9d469f04e3bae04edf1
Tasmiya Ansari <tasmiyansari2002@gmail.com>
2026-08-18 08:55:49 -0700

    Add quiz database migration and PostgreSQL configuration

--- app/settings.py
+++ app/settings.py
@@ -85,11 +85,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 import dj_database_url
-
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
