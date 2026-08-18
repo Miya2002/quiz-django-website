94b442d754098106578491f99103a3a93aae3b49
Tasmiya Ansari <tasmiyansari2002@gmail.com>
2026-08-18 09:21:09 -0700

    Connect Django to Neon PostgreSQL

--- app/settings.py
+++ app/settings.py
@@ -89,10 +89,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 import dj_database_url
 
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
